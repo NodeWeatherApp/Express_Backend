c4c717a016538942266baff5ded9915a80170507
Deion Shallenberger <shalledj6410@student.laccd.edu>
2021-12-03 16:20:15 -0800

    Create UserCreation API and rework weather data to append and send all data

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require("express");
 const app = express();
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
-
-var cors = require('cors');
-app.use(cors());
-
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const port = process.env.PORT || 5000;
+var cors = require("cors");
 
-// Extended: https://swagger.io/specification/#infoObject
+app.use(express.json());
+
 const swaggerOptions = {
   swaggerDefinition: {
     info: {
@@ -24,21 +23,23 @@ const swaggerOptions = {
   // ['.routes/*.js']
   apis: ["app.js"],
 };
-
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use(cors());
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "http://localhost:3000/", //original url
+    changeOrigin: true,
+    //secure: false,
+    onProxyRes: function (proxyRes, req, res) {
+      proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+    },
+  })
+);
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
-app.use('/api', createProxyMiddleware({ 
-  target: 'http://localhost:3000/', //original url
-  changeOrigin: true, 
-  //secure: false,
-  onProxyRes: function (proxyRes, req, res) {
-     proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-  }
-}));
-
-// Routes
+//Swagger Doc WeatherForecast API
 /**
  *  @swagger
  *  /WeatherForecastController:
@@ -49,13 +50,28 @@ app.use('/api', createProxyMiddleware({
  *          description: A successful response
  */
 
-app.get("/WeatherForecastController", (req, res) => {
-  function getRandomArbitrary(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-  temperatureC = getRandomArbitrary(-20, 55);
-  var currentdate = new Date();
-  var datetime =
+function getRandomWeather(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+function getRandomForecast() {
+  let forecast = [
+    "Freezing",
+    "Bracing",
+    "Chilly",
+    "Cool",
+    "Mild",
+    "Warm",
+    "Balmy",
+    "Hot",
+    "Sweltering",
+    "Scorching",
+  ];
+  let random_val = Math.floor(Math.random() * forecast.length);
+  return forecast[random_val];
+}
+function getDatetime() {
+  let currentdate = new Date();
+  let datetime =
     "Last Sync: " +
     currentdate.getDate() +
     "/" +
@@ -69,35 +85,75 @@ app.get("/WeatherForecastController", (req, res) => {
     ":" +
     currentdate.getSeconds();
 
-  var forecast = [
-    "Freezing",
-    "Bracing",
-    "Chilly",
-    "Cool",
-    "Mild",
-    "Warm",
-    "Balmy",
-    "Hot",
-    "Sweltering",
-    "Scorching",
-  ];
+  return datetime;
+}
+function getID() {
+  return id++;
+}
+const weather = [];
+let id = 1;
 
-  function getRandomForecast(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
+app.get("/WeatherForecastController", (req, res) => {
+  const currentWeather = {
+    id: getID(),
+    forecast: getRandomForecast(),
+    temperature: getRandomWeather(-20, 55),
+    date: getDatetime(),
+  };
+  console.log(currentWeather);
 
-  const weatherData = [
-    {
-    "id" : 1,
-    "forecast": (forecast[getRandomForecast(0,forecast.length)]),
-    "temperature": (temperatureC = getRandomArbitrary(-20, 55)),
-    "date": datetime,
-  }
-  ];
+  weather.push(currentWeather);
+  JSON.stringify(weather);
+  res.status(200).send(weather);
+});
 
-  console.log(weatherData);
+//Swagger Doc POST CreateUser API
+/**
+ *  @swagger
+ *  /CreateUser:
+ *    post:
+ *      description: Post User To Database
+ *      parameters:
+ *        - in: body
+ *          name: UserName
+ *          description: The user to create.
+ *          schema:
+ *            type: object
+ *            required:
+ *              - userName
+ *            properties:
+ *              userName:
+ *                type: string
+ *              password:
+ *                type: string
+ *      responses:
+ *        '201':
+ *          description: Created User
+ */
+const users = [];
 
-  res.status(200).send(weatherData);
+app.post("/CreateUser", (req, res) => {
+  const user = {userName: req.body.userName, password: req.body.password }
+  users.push(user);
+  console.log(users);
+
+  res.status(201).send('Success');
+});
+
+// Extended: https://swagger.io/specification/#infoObject
+
+/**
+ *  @swagger
+ *  /login:
+ *    post:
+ *      description: Use to Login
+ *      responses:
+ *        '200':
+ *          description: A successful response
+ */
+
+app.post("/Login", (req, res) => {
+  res.status(204).send(`Username: ${firstname} Password: ${lastname}`);
 });
 
 app.listen(port, () => {
