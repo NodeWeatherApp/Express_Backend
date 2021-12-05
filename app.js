b1e5f6d584dca9bbf00ace36607d8b8262572a35
Deion Shallenberger <shalledj6410@student.laccd.edu>
2021-12-05 13:11:03 -0800

    createDBConnection

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
+// allows env variables to be set on process.env
+require("dotenv").config(); 
+
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
 
-const { createProxyMiddleware } = require("http-proxy-middleware");
-var cors = require("cors");
-
-const userRoutes = require('./routes/userRoutes.js');
-const weatherRoutes = require('./routes/weatherRoutes');
-
-app.use(express.json());
-
+// swagger docs
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
-
-
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 const swaggerOptions = {
   swaggerDefinition: {
     info: {
@@ -29,13 +23,14 @@ const swaggerOptions = {
   },
   apis: ['./routes/*.js'],
 };
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // cors
+var cors = require("cors");
 app.use(cors());
 
 // middleware
+const { createProxyMiddleware } = require("http-proxy-middleware");
 app.use(
   "/api",
   createProxyMiddleware({
@@ -47,17 +42,18 @@ app.use(
     },
   })
 );
-
-// swagger docs
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use(express.json()); // parse json bodies in request object
 
 // weather routes
+const weatherRoutes = require('./routes/weatherRoutes');
 app.use('/weather',weatherRoutes);
 
 // user routes
+const userRoutes = require('./routes/userRoutes.js');
 app.use('/user',userRoutes);
 
-// server port
+// listen on port
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
---
+++ models/userModel.js
@@ -0,0 +1,9 @@
+module.exports = {
+    fields: {
+        name        : "text",
+        email       : "text",
+        password    : "text",
+        created     : "timestamp"
+    },
+    key: ["name"]
+}
