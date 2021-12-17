26b60a9d2eae5034ae1843023c33d34e305fe8f3
Deion Shallenberger <51349201+upDJ@users.noreply.github.com>
2021-12-17 13:22:39 -0800

    Dtos (#14)
    * created new controller from sequelize methods. created one to many realtionships for weather and location tables
    
    * Able to add child table with parent's foreign key
    
    * stopped forceful migrations. updated controllers to match ORM models
    
    * fetching data w/ one to many mapping of foreign key locationId in weather table
    
    Co-authored-by: Deion Shallenberger <shalledj6410@student.laccd.edu>

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -2,21 +2,38 @@ const randomWeather = require("../handlers/weatherHandler");
 const Weather = require("../models/Weather");
 
 exports.weather_create = async (req, res, next) => {
-  const { forecast, temperature, date, locationId } = req.body;
+  try {
+    const { forecast, temperature, date, locationId } = req.body;
 
-  // Create new location
-  const weather = await Weather.create({
-    forecast,
-    temperature,
-    date,
-    locationId,
-  })
-    .then(console.log(weather))
-    .catch((err) => {
-      console.log(err);
-      next(err);
-    });
+    // Create new location
+    const weather = await Weather.create({
+      forecast,
+      temperature,
+      date,
+      locationId,
+    })
+      .then(console.log("weather object created"))
+      .catch((err) => {
+        console.log(err);
+        next(err);
+      });
 
-  res.status(201).json({ created: weather.dataValues });
+    res.status(201).json({ created: weather.dataValues });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
+exports.weather_get_all = async (req, res, next) => {
+  Weather.findAll({
+    where: {
+      locationId: req.params.locationId,
+    },
+  })
+    .then((weather) => {
+      console.log(weather);
+      res.status(200).json({ response: "success", weather: weather });
+    })
+    .catch((err) => console.log(err));
+};
