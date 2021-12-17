992dce0d227677c7d49daea3d79f30de85d9df3d
Deion Shallenberger <51349201+upDJ@users.noreply.github.com>
2021-12-16 23:28:06 -0800

    update user model and weather model to meet ORM standards (#10)
    Co-authored-by: Deion Shallenberger <shalledj6410@student.laccd.edu>

--- models/weatherModel.js
+++ models/weatherModel.js
@@ -1,49 +1,16 @@
+const Sequelize = require("sequelize");
 const db = require("../config/db");
 
-class Weather {
-  constructor(forecast, temperature, date) {
-    this.forecast = forecast;
-    this.temperature = temperature;
-    this.date = date;
-  }
-
-  save() {
-    let d = new Date();
-    let yyyy = d.getFullYear();
-    let mm = d.getMonth() + 1;
-    let dd = d.getDate();
-
-    let createdAtDate = `${yyyy}-${mm}-${dd}`;
-
-    let sql = `
-            INSERT INTO weather(
-                forecast,
-                temperature,
-                date,
-                created_at
-            )
-            VALUES(
-                "${this.forecast}",
-                "${this.temperature}",
-                "${this.date}",
-                "${createdAtDate}"
-            );`;
-
-    db.execute(sql);
-    return {
-      weather: {
-        forecast: this.forecast,
-        temperature: this.temperature,
-        date: this.date,
-        created_at: createdAtDate,
-      },
-    };
-  }
-  static findAll() {
-    let sql = "SELECT * FROM weather;";
-
-    return db.execute(sql);
-  }
-}
+const Weather = db.define("weather", {
+  forecast: {
+    type: Sequelize.STRING,
+  },
+  temperature: {
+    type: Sequelize.STRING,
+  },
+  date: {
+    type: Sequelize.STRING,
+  },
+});
 
 module.exports = Weather;
