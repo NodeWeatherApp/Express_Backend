3c44f56c2523d1848accfd0092cb1b9cbf0ab5a0
Deion Shallenberger <shalledj6410@student.laccd.edu>
2021-12-20 19:04:22 -0800

    deployment issues fixed

--- config/db.js
+++ config/db.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const Sequelize = require("sequelize");
 
 module.exports = new Sequelize(
-  `${heroku_5cff7007787fc23}`,
-  `${b42e921b82cdf9}`,
-  `${c89424bf}`,
+  "heroku_5cff7007787fc23",
+  "b42e921b82cdf9",
+  "c89424bf",
   {
-    host: `${us-cdbr-east-05.cleardb.net}`,
+    host: "us-cdbr-east-05.cleardb.net",
     dialect: "mysql",
   }
 );
